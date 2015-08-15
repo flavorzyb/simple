ed5286e8eb00c8448ef236dd4d1b474150dc0729
flavorzhu <haker-haker@163.com>
2015-08-15 09:35:06 +0800

    add Repository && Helper test case

---
+++ tests/config/RepositoryTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flavor
+ * Date: 15/8/15
+ * Time: 上午9:25
+ */
+
+namespace Simple\Config;
+
+
+class RepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testOptionIsMutable()
+    {
+        $config = new Repository();
+        $this->assertEquals([], $config->all());
+
+        $data   = ["key" => 111, "key2" => 22222];
+        $config = new Repository($data);
+        $this->assertEquals($data, $config->all());
+
+        $this->assertEquals($data['key'], $config['key']);
+        $this->assertTrue($config->offsetExists("key"));
+
+        unset($config['key']);
+        $this->assertFalse($config->offsetExists('key'));
+    }
+}
