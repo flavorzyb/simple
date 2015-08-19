a9459727f7b1e9c3baf10a0c161ff78e74466089
flavorzhu <haker-haker@163.com>
2015-08-19 10:54:00 +0800

    update

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -222,9 +222,30 @@ class Application
      */
     public function bootStrap()
     {
-        $this->initConfig();
+        error_reporting(-1);
+        set_error_handler([$this, 'handleError']);
 
+        $this->initConfig();
         $this->initEnvironment();
+    }
+    /**
+     * Convert a PHP error to an ErrorException.
+     *
+     * @param  int  $level
+     * @param  string  $message
+     * @param  string  $file
+     * @param  int  $line
+     * @param  array  $context
+     * @return void
+     *
+     * @throws \ErrorException
+     */
+    public function handleError($level, $message, $file = '', $line = 0, $context = array())
+    {
+        if (error_reporting() & $level)
+        {
+            throw new \ErrorException($message, 0, $level, $file, $line);
+        }
     }
 
     /**
