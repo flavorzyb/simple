37a57531a0bfd18bc02dd2603dd4703245227901
flavorzhu <haker-haker@163.com>
2016-07-08 02:15:39 +0800

    add DotEnv test case

--- tests/Environment/DotEnvTest.php
+++ tests/Environment/DotEnvTest.php
@@ -9,9 +9,20 @@
 namespace Environment;
 
 
+use Simple\Environment\DotEnv;
+
 class DotEnvTest extends \PHPUnit_Framework_TestCase
 {
     public function testOptions() {
+        $env = new DotEnv(__DIR__, ".env");
+        $data = $env->load();
+        $this->assertTrue(sizeof($data) == 8);
 
+        $data = $env->overLoad();
+        $this->assertTrue(sizeof($data) == 8);
+
+        $env = new DotEnv(__DIR__, 222);
+        $data = $env->load();
+        $this->assertTrue(sizeof($data) == 8);
     }
 }
